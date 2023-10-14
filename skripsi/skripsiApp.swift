0154c79c0a5aefe1f1c128eb354017dd020810a8
fhassidiqi <94043950+falahhasbiassidiqi@users.noreply.github.com>
2023-10-14 15:40:11 +0700

    Initial Commit

---
+++ skripsi/skripsiApp.swift
@@ -0,0 +1,17 @@
+//
+//  skripsiApp.swift
+//  skripsi
+//
+//  Created by Falah Hasbi Assidiqi on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct skripsiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
